4e57fe2d03b63571d7ffdf93caa43419c478315d
Cacho Medina <zanlorenzocacho10@gmail.com>
2024-01-19 17:25:58 -0300

    arreglos

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ function App() {
     return (
         <main>
             <Mensaje></Mensaje>
-            <Mensaje2 mensaje={"amigazoo"}></Mensaje2>
-            <Mensaje3 mensaje={"amigo"}></Mensaje3>
+            <Mensaje2 mensaje={"mundoo"}></Mensaje2>
+            <Mensaje3 mensaje={"mundooo"}></Mensaje3>
         </main>
     );
 }
