5d3f54b73dc7d3d622076de0672d08b8c7947e1e
Cacho Medina <zanlorenzocacho10@gmail.com>
2024-02-01 04:57:08 -0300

    separando la app en 3 repos

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import "./App.css";
 import Mensaje from "./components/Mensaje";
-import Mensaje2 from "./components/Mensaje2";
-import Mensaje3 from "./components/Mensaje3";
+
 function App() {
     return (
         <main>
             <Mensaje></Mensaje>
-            <Mensaje2 mensaje={"mundoo"}></Mensaje2>
-            <Mensaje3 mensaje={"mundooo"}></Mensaje3>
+            
         </main>
     );
 }
--- src/components/Mensaje2.jsx
+++
@@ -1,9 +0,0 @@
-function Mensaje2({ mensaje }) {
-    return (
-        <div>
-            <h1>Hola {mensaje}</h1>
-        </div>
-    );
-}
-
-export default Mensaje2;
